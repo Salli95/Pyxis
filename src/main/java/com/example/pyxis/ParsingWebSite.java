e9de10774b1d54ff78997a61a9ad43edcd908aaa
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-12-05 12:27:04 +0500

    Заработало подключение к серверу.Проверяю через парсертест.Нужно теперь настроить выбор города  и сохранение необходимых полей

--- src/main/java/com/example/pyxis/ParsingWebSite.java
+++ src/main/java/com/example/pyxis/ParsingWebSite.java
@@ -8,21 +8,39 @@ import org.jsoup.select.Elements;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.*;
+import java.io.*;
 
 import java.io.IOException;
 
 public class ParsingWebSite {
     public void parseData() throws IOException {
 
+        String apiKey = "12556c21f1bc4b493c409a2c6fcedeb2";  //api не  трогать .Я его получил через регистрацию
+        //эти поля это координаты где нужно узнать погоду(сейчас стоит нюйорк вроде)
+        String lat = "40.7128";
+        String lon = "-74.0060";
 
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
 
-        String apiKey = "12556c21f1bc4b493c409a2c6fcedeb2";
-        String url = "https://api.weather.com/v3/current/weather?apiKey=" + apiKey + "&units=m&language=ru-RU&geocode=40.7128,-74.0060";
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-        Connection.Response response = Jsoup.connect(url).ignoreHttpErrors(true).execute();
-        String json = response.body();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
 
-        System.out.println(json);
+            String responseBody = response.toString();
+            System.out.println("Ответ от API: " + responseBody);
+        } else {
+            System.out.println("Ошибка: " + responseCode);
+        }
+    }
 
     }
-}
