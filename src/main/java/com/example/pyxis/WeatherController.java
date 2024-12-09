825e166d7e39f2801fa039945b706d35e0d2c504
James River <gerojlordrana@gmail.com>
2024-12-09 18:22:11 +0500

    ParsingWebSite.java

--- src/main/java/com/example/pyxis/WeatherController.java
+++ src/main/java/com/example/pyxis/WeatherController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 import java.sql.*;
@@ -13,6 +15,9 @@ public class WeatherController {
 
     @FXML
     private Button Update;
+
+    @FXML
+    private ImageView weatherIcon;
 
     @FXML
     private HBox Weather_output;  //вывод погоды в это поле
@@ -37,10 +42,9 @@ public class WeatherController {
 
 @FXML
 public void selectCity() throws SQLException, ClassNotFoundException {
-    // Подключение к базе данных
     Connection connection = new Conection().getDbConnection();
 
-    String sql = "SELECT city, forecast_time FROM " + DatabaseFields.Table_weather + " LIMIT 1";
+    String sql = "SELECT city, forecast_time, weather_icon FROM " + DatabaseFields.Table_weather + " LIMIT 1";
 
     try (PreparedStatement statement = connection.prepareStatement(sql);
          ResultSet resultSet = statement.executeQuery()) {
@@ -54,9 +58,16 @@ public void selectCity() throws SQLException, ClassNotFoundException {
             if (lastUpdated != null) {
                 // Преобразуем время в строку для отображения
                 String formattedTime = new SimpleDateFormat("dd.MM.yyyy \n HH:mm:ss").format(lastUpdated);
-                time_of_update.setText("Last update: \n" +  formattedTime);
+                time_of_update.setText("Last update: \n" + formattedTime);
             } else {
                 output.setText("Время обновления не доступно.");
+            }
+            String iconCode = resultSet.getString("weather_icon");
+            if (iconCode != null) {
+                String imageUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                weatherIcon.setImage(new javafx.scene.image.Image(imageUrl));
+            } else {
+                output.setText("Иконка погоды отсутствует.");
             }
         } else {
             output.setText("Данные отсутствуют.");
