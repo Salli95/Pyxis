80eaf2c7d5cb75e9290e466163acbc44289c87e6
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-12-10 21:36:38 +0500

    Сделал вывод иконок в консоль.И встроил метод показа города и последнего обновления с иконками старых даных до update

--- src/main/java/com/example/pyxis/Weather.java
+++ src/main/java/com/example/pyxis/Weather.java
@@ -1,24 +1,38 @@
 package com.example.pyxis;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Weather extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException, ClassNotFoundException {
         FXMLLoader fxmlLoader = new FXMLLoader(Weather.class.getResource("WeatherView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Weather App");
         stage.setScene(scene);
         stage.show();
+        WeatherController controller = fxmlLoader.getController();
+        Platform.runLater(() -> {
+            try {
+                controller.selectCity();
+                controller.displayWeatherIcons();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch();
+
     }
 
 
--- src/main/java/com/example/pyxis/WeatherController.java
+++ src/main/java/com/example/pyxis/WeatherController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.Image;
+
 
 import java.io.IOException;
 import java.sql.*;
@@ -31,13 +31,35 @@ public class WeatherController {
     @FXML
     private Label time_of_update;
 
+
+    @FXML
+    private ImageView day1ImageView;
+
+    @FXML
+    private ImageView day2ImageView;
+
+    @FXML
+    private ImageView day3ImageView;
+
+    @FXML
+    private ImageView day4ImageView;
+
+    @FXML
+    private ImageView day5ImageView;
+
+    @FXML
+    public void initialize() throws SQLException, ClassNotFoundException {
+        selectCity();
+    }
+
+
     @FXML
     private void updateWeather() throws IOException, SQLException, ClassNotFoundException {
         ParsingWebSite parsingWebSite = new ParsingWebSite();
         parsingWebSite.parseData();
         selectCity();
-
         output.setText("Погода успешно обновлена на ближайшие 5 дней.");
+        displayWeatherIcons();
     }
 
 @FXML
@@ -62,13 +84,16 @@ public void selectCity() throws SQLException, ClassNotFoundException {
             } else {
                 output.setText("Время обновления не доступно.");
             }
-            String iconCode = resultSet.getString("weather_icon");
-            if (iconCode != null) {
-                String imageUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-                weatherIcon.setImage(new javafx.scene.image.Image(imageUrl));
-            } else {
-                output.setText("Иконка погоды отсутствует.");
-            }
+
+//            String iconCode = resultSet.getString("weather_icon");
+//            if (iconCode != null) {
+//                String imageUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+//                weatherIcon.setImage(new javafx.scene.image.Image(imageUrl));
+//            } else {
+//                output.setText("Иконка погоды отсутствует.");
+//            }
+
+
         } else {
             output.setText("Данные отсутствуют.");
         }
@@ -78,7 +103,48 @@ public void selectCity() throws SQLException, ClassNotFoundException {
 }
 
 
+    @FXML
+    public void displayWeatherIcons() throws SQLException, ClassNotFoundException {
+        Connection connection = new Conection().getDbConnection();
 
+        String sql = "SELECT weather_icon FROM " + DatabaseFields.Table_weather + " ORDER BY forecast_time LIMIT 5";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            int dayIndex = 0;
+            while (resultSet.next()) {
+                String iconCode = resultSet.getString("weather_icon");
+                if (iconCode != null) {
+                    String imageUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                    ImageView iconImageView = getIconImageView(dayIndex);
+                    iconImageView.setImage(new javafx.scene.image.Image(imageUrl));
+                } else {
+                    output.setText("Иконка погоды отсутствует.");
+                }
+                dayIndex++;
+            }
+        } catch (SQLException e) {
+            output.setText("Ошибка: " + e.getMessage());
+        }
+    }
+
+    private ImageView getIconImageView(int dayIndex) {
+        switch (dayIndex) {
+            case 0:
+                return day1ImageView;
+            case 1:
+                return day2ImageView;
+            case 2:
+                return day3ImageView;
+            case 3:
+                return day4ImageView;
+            case 4:
+                return day5ImageView;
+            default:
+                return null;
+        }
+    }
 
 
 
